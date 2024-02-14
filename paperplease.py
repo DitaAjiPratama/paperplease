56f94136da78ebb9d5696290a70911b57fa33b8e
ditaajipratama <ditaajipratama@protonmail.com>
2024-02-14 07:21:01 +0700

    add token_get and token_out

--- paperplease.py
+++ paperplease.py
@@ -3,6 +3,12 @@ import cherrypy
 def token_set(key, value):
     cherrypy.session[key] = value
 
+def token_get(key):
+    return cherrypy.session[key]
+
+def token_out():
+    cherrypy.lib.sessions.expire()
+
 def token_check(key, redirect):
     if cherrypy.session.get(key) == None:
         raise cherrypy.HTTPRedirect(redirect)
