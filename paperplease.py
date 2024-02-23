b588e03978568e2e3293fcc06e64d93e8805df8d
ditaajipratama <ditaajipratama@protonmail.com>
2024-02-23 21:58:35 +0700

    add a False return for an empty token_get

--- paperplease.py
+++ paperplease.py
@@ -4,7 +4,10 @@ def token_set(key, value):
     cherrypy.session[key] = value
 
 def token_get(key):
-    return cherrypy.session[key]
+    if cherrypy.session.get(key) == None:
+        return False
+    else:
+        return cherrypy.session[key]
 
 def token_out():
     cherrypy.lib.sessions.expire()
